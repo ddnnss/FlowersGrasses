2a006d4ad88ca76be0a5edff28a180a61f9e4b6f
ddnnss <ddnnss.i1@gmail.com>
2019-06-16 22:22:38 +0300

    new item model

--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
-from items.models import Category
+from items.models import Category,Collection
 
 # Create your views here.
 def index(request):
     all_category = Category.objects.all()
+    all_collections = Collection.objects.all()
     return render(request, 'pages/index.html', locals())
 
