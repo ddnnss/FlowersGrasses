d08edffde82f51aba9f45dea2c5ef41d580b4fe6
qqw  wee <a@a.ru>
2019-06-18 08:34:55 +0300

    items fix

--- items/urls.py
+++ items/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
 
     path('item/<item_slug>', views.product, name='product'),
     path('category/<category_slug>', views.cat, name='cat'),
+    path('collection/<collection_slug>', views.collection, name='collection'),
 
 
 
--- items/views.py
+++ items/views.py
@@ -11,3 +11,9 @@ def cat(request,category_slug):
     filters = Filter.objects.filter(category=category)
     all_items = Item.objects.filter(category=category)
     return render(request, 'pages/category.html', locals())
+
+
+def collection(request,collection_slug):
+    collection = Collection.objects.get(name_slug=collection_slug)
+    all_items = Item.objects.filter(collection=collection)
+    return render(request, 'pages/collection.html', locals())
